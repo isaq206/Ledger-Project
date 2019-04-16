0002742ed6597aa7a1e4d75f206a09e6fbc9b211
isaq206 <im.sloan95@gmail.com>
2019-04-15 22:06:17 -0600

    I've tweaked the update, create new, and review methods. Will look at accounting for spaces in entried next.

--- finances.cpp
+++ finances.cpp
@@ -6,28 +6,29 @@ using namespace std;
 
 //a comment to test git
 
-void grabInFile(string & fileName) {
+void grabInFile(string & fileName) 
+{
    cout << "Enter record name: ";
    cin >> fileName;
 
 }
 
-void readInFile(string fileName, vector <string> &recordContent) throw (const char*) {
+void readInFile(string fileName, vector <string> &recordContent) throw (const char*) 
+{
    ifstream fin;
    fin.open(fileName);
 
-   if (fin.fail()) {
+   if (fin.fail())
       throw ("Error: Cannot read file.");
-   }
 
-   if (!recordContent.empty()) {
+   if (!recordContent.empty())
       recordContent.clear();
-   }
 
    string content;
 
    int index = 0;
-   while(fin >> content) {
+   while(fin >> content) 
+   {
       recordContent.push_back(content);
       cout << recordContent[index] << endl;
       index++;
@@ -36,16 +37,24 @@ void readInFile(string fileName, vector <string> &recordContent) throw (const ch
    fin.close();
 }
 
-void updateData(vector <string> &recordContent) {
+void updateData(vector <string> &recordContent) 
+{
    string data;
 
-   cout << "Enter data: ";
-   cin >> data;
-
-   recordContent.push_back(data);
+   cout << "*NOTE: You can only file with 100 things at a time. Please enter \'done\' when finished updating.\n";
+   cin.ignore();
+   do
+   {
+      cout << "Enter data: ";
+      cin >> data;
+      if(data != "done")
+         recordContent.push_back(data);
+   }
+   while(data != "done");
 }
 
-void saveData(string fileName, vector <string> recordContent) throw (const char*) {
+void saveData(string fileName, vector <string> recordContent) throw (const char*) 
+{
    ofstream fout;
    fout.open(fileName);
 
@@ -54,19 +63,41 @@ void saveData(string fileName, vector <string> recordContent) throw (const char*
    }
 
    int counter = 0;
-   for (vector <string>::iterator it = recordContent.begin(); it != recordContent.end(); it++, counter++) { 
+   for (vector <string>::iterator it = recordContent.begin(); it != recordContent.end(); it++, counter++) 
+   { 
       fout << recordContent[counter] << " ";
    }
 
    fout.close();
 }
 
-int main() {
+void createData(vector <string> & recordContent, string & fileName)
+{
+   string newData; //for when a new file is made
 
+   cout << "Please enter new data.\n";
+   cin.ignore();
+   getline(cin, newData);
+
+   recordContent.clear();
+   try
+   {
+      recordContent.push_back(newData);
+   }
+   catch(std::bad_alloc)
+   {
+      cout << "Cannot push_back new data" << endl;
+   }
+   grabInFile(fileName);
+   cout << endl;
+   saveData(fileName, recordContent);
+}
+
+int main() 
+{
    //DATA-CONTENT VARIABLES
    string fileName = "\0";
    vector <string> recordContent;
-   string newData; //for when a new file is made
 
 
    //INPUT VARIABLES
@@ -82,28 +113,14 @@ int main() {
       switch(choice) {
          case 'N':
          case 'n':
-            cout << "Please enter new data.\n";
-            cin.ignore();
-            getline(cin, newData);
-            cin.ignore();
-            recordContent.clear();
-            try
-            {
-               recordContent.push_back(newData);
-            }
-            catch(std::bad_alloc)
-            {
-               cout << "Cannot push_back new data" << endl;
-            }
-            cout << "What is the name of new file?\n";
-            cin >> fileName;
-            saveData(fileName, recordContent);
+            createData(recordContent, fileName);
             break;
          case 'L':
          case 'l':
-            try {
-            grabInFile(fileName);
-            readInFile(fileName, recordContent);
+            try 
+            {
+               grabInFile(fileName);
+               readInFile(fileName, recordContent);
             }
             catch (const char* e)
             {
@@ -116,9 +133,12 @@ int main() {
             break;
          case 'R':
          case 'r':
-            for (vector <string>::iterator it = recordContent.begin(); it != recordContent.end(); it++) {
-               cout << *it << endl;
+            for (vector <string>::iterator it = recordContent.begin(); it != recordContent.end(); it++) 
+            {
+               cout << *it << " - THIS STATEMENT IS FOR DEBUGGING PURPOSES" << endl;
+               
             }
+            cout << endl;
             break;
          case 'S':
          case 's':
